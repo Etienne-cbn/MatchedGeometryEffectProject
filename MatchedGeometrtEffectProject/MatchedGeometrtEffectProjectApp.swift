177d620c1307c193c68eef11b48209538a07a632
Etienne <e.chambon-ext@kreactive.com>
2023-11-12 00:34:23 +0100

    Initial Commit

---
+++ MatchedGeometrtEffectProject/MatchedGeometrtEffectProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometrtEffectProjectApp.swift
+//  MatchedGeometrtEffectProject
+//
+//  Created by Etienne on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometrtEffectProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
